e1e7024022e44f7bba149e318959f17724c50fd5
MarioSeiler <mario.seiler@sunrise.ch>
2020-06-16 15:20:33 +0200

    Ou nüt

--- templates/default/about.php
+++ templates/default/about.php
@@ -1,1 +1,3 @@
-Outerspass ist eine Webseite, auf welcher man Games kaufen kann. Wir haben die neusten Spiele vor jedem anderen Store, zu exklusiven Preisen.
+Outerspass ist eine Webseite, auf welcher man Games kaufen kann. Wir haben die neusten Spiele vor jedem anderen Store, zu exklusiven Preisen. <br>
+Wir bitten Spiele von Shootern bis zu Strategie oder Co-op. <br><br>
+Outerspass-Store wurde 2020 gegründet und hat schon über 1 Millionen einkäufe im ersten Monat erzielt. Unser Store ist einfach zu gebrauchen und sehr simpel 
